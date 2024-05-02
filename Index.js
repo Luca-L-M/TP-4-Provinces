d463d7fa8cf611f258d518deb441c406df47140e
Gabriel Guivisdalsky <gguivisdalsky@gmail.com>
2024-05-02 12:08:42 -0300

    index

---
+++ Index.js
@@ -0,0 +1,23 @@
+import express from"express";
+import cors from"cors";
+import ProvinceRouter from"./src/controllers/province-controller.js";
+
+constapp=express();
+constport=3000; //El puerto 3000 (http://localhost:3000)
+
+//Agrego los Middlewares
+app.use(cors()); //Middleware de CORS.
+app.use(express.json()); //Middleware para parsear y comprender JSON.
+
+//Endpoints (todos los Routers)
+app.use("/api/province",ProvinceRouter);
+
+//Inicio el Server y lo pongo a escuchar.
+app.listen(port,()=> {
+    console.log(`Example app listening on port${port}`)
+})
+
+//Endpoints funcionales
+app.get("/api/province", (req, res) => {
+    res.status(200).send();
+})
