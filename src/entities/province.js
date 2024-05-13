81bb7b71db53dc7a323c9073454b122554b66433
LUCA LISTINGART MAEKANEHISA <47805160@ort>
2024-05-13 09:04:06 -0300

    constructores

--- src/entities/province.js
+++ src/entities/province.js
@@ -7,4 +7,23 @@ class Province{
     display_order;
 }
 
+constructor(id, name, full_name, latitud, longitud, display_order)
+{
+    this.id = id;
+    this.name = name;
+    this.full_name = full_name;
+    this.latitud = latitud;
+    this.longitud = longitud;
+    this.display_order = display_order;
+}
+
+constructor(name, full_name, latitud, longitud, display_order)
+{
+    this.name = name;
+    this.full_name = full_name;
+    this.latitud = latitud;
+    this.longitud = longitud;
+    this.display_order = display_order;
+}
+
 export default Province;
