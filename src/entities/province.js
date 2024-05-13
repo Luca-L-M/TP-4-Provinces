a7bff39bdb589da87fa4c252ddc972859a35d9cc
LUCA LISTINGART MAEKANEHISA <47805160@ort>
2024-05-13 08:28:49 -0300

    falta el body e postman

---
+++ src/entities/province.js
@@ -0,0 +1,10 @@
+class Province{
+    id;
+    name;
+    full_name;
+    latitud;
+    longitud;
+    display_order;
+}
+
+export default Province;
--- src/repositories/provinces-repositories.js
+++ src/repositories/provinces-repositories.js
@@ -49,7 +49,45 @@ export default class ProvinceRepository
         try
         {
             await client.connect();
-            const sql = `SELECT * FROM provinces where id = ${id}`;
+            const sql = `Insert into Province(name, full_name, latitud, longitud, display_order) Values (${entity.name}, ${entity.full_name}, ${entity.latitud}, ${entity.longitud}, ${entity.display_order})`;
+            const result = await client.query(sql);
+            await client.end();
+            returnArray = result.rows;
+        }
+        catch (error)
+        {
+            console.log(error);
+        }
+        return returnArray;
+    }
+
+    updateAsync = async (entity) =>
+    {
+        let returnArray = null;
+        const client = new Client(DBConfig);
+        try
+        {
+            await client.connect();
+            const sql = `Update provinces Set name=${entity.name}, full_name=${entity.full_name}, latitud=${entity.latitud}, longitud=${entity.longitud}, display_order=${entity.display_order} Where id = ${entity.id}`;
+            const result = await client.query(sql);
+            await client.end();
+            returnArray = result.rows;
+        }
+        catch (error)
+        {
+            console.log(error);
+        }
+        return returnArray;
+    }
+
+    deleteByIdAsync = async (id) =>
+    {
+        let returnArray = null;
+        const client = new Client(DBConfig);
+        try
+        {
+            await client.connect();
+            const sql = `Delete * FROM provinces where id = ${id}`;
             const result = await client.query(sql);
             await client.end();
             returnArray = result.rows;
